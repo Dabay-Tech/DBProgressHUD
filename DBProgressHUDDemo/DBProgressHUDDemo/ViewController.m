277f233916ddc264bf54d26ef161ae3b019004ba
Daonkey-Tao <taofei0610@gmail.com>
2017-07-21 11:35:25 +0800

    change showsuccess and showerror

--- DBProgressHUDDemo/DBProgressHUDDemo/ViewController.m
+++ DBProgressHUDDemo/DBProgressHUDDemo/ViewController.m
@@ -22,19 +22,19 @@
 //    [DBProgressHUD db_showLoading:@"  加载中...   " toView:self.view];
     
 //    [DBProgressHUD db_showError:@"服务暂不可用，请稍后重试"];
-    [DBProgressHUD db_showSuccess:@" 请求成功    "];
+//    [DBProgressHUD db_showSuccess:@"添加参保人成功"];
     
 
 //    [DBProgressHUD db_showLoading:@"  加载中...   " toView:nil];
     
 //    [DBProgressHUD db_showMessage:@"Dabay-Tech"];
     
-//    [DBProgressHUD db_showError:@"服务暂不可用，请稍后重试"];
+
     
     
 //    [DBProgressHUD db_showMultiLineMessage:@"服务暂不可用，请稍后\n重试。服务暂不可用，请稍后重试" toView:nil];
     
-//    [DBProgressHUD db_showSuccess:@"大白科技-DBProgressHUD" toView:nil];
+    [DBProgressHUD db_showSuccess:@"大白科技-DBProgressHUD" toView:nil];
     
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{

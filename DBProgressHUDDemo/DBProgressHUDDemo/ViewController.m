8b552e27dc6d2e930454cfd1fd842a1f7e96c8bf
Daonkey-Tao <taofei0610@gmail.com>
2017-07-21 17:20:02 +0800

    change success and error icon

--- DBProgressHUDDemo/DBProgressHUDDemo/ViewController.m
+++ DBProgressHUDDemo/DBProgressHUDDemo/ViewController.m
@@ -19,10 +19,11 @@
     [super viewDidLoad];
     
     
+    self.view.backgroundColor = [UIColor lightGrayColor];
 //    [DBProgressHUD db_showLoading:@"  加载中...   " toView:self.view];
     
 //    [DBProgressHUD db_showError:@"服务暂不可用，请稍后重试"];
-//    [DBProgressHUD db_showSuccess:@"添加参保人成功"];
+    [DBProgressHUD db_showSuccess:@"添加参保人成功"];
     
 
 //    [DBProgressHUD db_showLoading:@"  加载中...   " toView:nil];
@@ -34,7 +35,7 @@
     
 //    [DBProgressHUD db_showMultiLineMessage:@"服务暂不可用，请稍后\n重试。服务暂不可用，请稍后重试" toView:nil];
     
-    [DBProgressHUD db_showSuccess:@"大白科技-DBProgressHUD" toView:nil];
+//    [DBProgressHUD db_showSuccess:@"大白科技-DBProgressHUD" toView:nil];
     
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
